e92ee8d0205557be65dfe86399b36b40eb574dc2
Alexander Eberle <alexander171294@live.com>
2014-08-13 09:31:17 -0300

    add wait in socket refresh

--- src/class/httpClient.php
+++ src/class/httpClient.php
@@ -65,8 +65,7 @@ class httpClient
 		var_dump($headers);
 		$this->socket->send($headers, false);
 		// esperamos la respuesta
-		sleep(5);
-		$this->socket->refresh();
+		while($this->socket->refresh() === false);
 	}	
 	
 	public function post($resources, $params, $headers = array('User-Agent' => 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0', 'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Lenguaje' => 'es-ar,es;q=0.8,en-us;q=0.5,en;q=0.3', 'Connection' => 'keep-alive'))
