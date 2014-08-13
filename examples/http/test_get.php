fb3a4812173e91b568e636f78d51f7c1e30a33a8
Alexander Eberle <alexander171294@live.com>
2014-08-13 08:55:59 -0300

    fix namespace implement httpClient

--- examples/http/test_get.php
+++ examples/http/test_get.php
@@ -3,7 +3,7 @@
 require('../../src/iSocketMaster.php');
 require('../../src/class/httpClient.php');
 
-$http = new httpClient('underc0de.org');
+$http = new PHPSocketMaster\httpClient('underc0de.org');
 
 // solicitamos el index.php
 $http->get('foro/index.php',array('board' => '131.0'));
