8fe05a6c4d2481f5592d15c276222ad06517606d
Alexander Eberle <alexander171294@live.com>
2014-08-13 11:14:45 -0300

    fix change sha1 concat

--- examples/http/test_post.php
+++ examples/http/test_post.php
@@ -8,7 +8,7 @@ require('../../src/class/httpClient.php');
 $http = PHPSocketMaster\httpClient::Factory('foro.infiernohacker.com', true);
 
 // solicitamos el index.php
-$http->post('index.php?action=login2',array('user' => 'alexmanycool', 'passwrd' => '', 'cookieneverexp'=>'on', 'hash_passwrd' => sha1('alex1234')));
+$http->post('index.php?action=login2',array('user' => 'alexmanycool', 'passwrd' => '', 'cookieneverexp'=>'on', 'hash_passwrd' => sha1('alexmanycoolalex1234')));
 
 // mostramos el cuerpo del mensaje recibido
 file_put_contents('log.out',$http->response['Main']);
