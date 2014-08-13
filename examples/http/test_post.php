bd5debba9239528d15e723db90fb9b9dd4b50f52
Alexander Eberle <alexander171294@live.com>
2014-08-13 11:43:46 -0300

    fix change password D:

--- examples/http/test_post.php
+++ examples/http/test_post.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * ejemplo para logguearse en un smf
+ */
+
 require('../../src/iSocketMaster.php');
 require('../../src/class/httpClient.php');
 
@@ -8,7 +12,10 @@ require('../../src/class/httpClient.php');
 $http = PHPSocketMaster\httpClient::Factory('foro.infiernohacker.com', true);
 
 // solicitamos el index.php
-$http->post('index.php?action=login2',array('user' => 'alexmanycool', 'passwrd' => '', 'cookieneverexp'=>'on', 'hash_passwrd' => sha1('alexmanycoolalex1234')));
+$cur_session = null;
+$user = 'alexmanycool';
+$pass = 'alex1234';
+$http->post('index.php?action=login2',array('user' => $user, 'passwrd' => '', 'cookieneverexp'=>'on', 'hash_passwrd' => sha1(sha1($user.$pass).$cur_session)));
 
 // mostramos el cuerpo del mensaje recibido
 file_put_contents('log.out',$http->response['Main']);
