ffe731f6ee32fcca67f56ce3e39a3e95ef9c068b
alexander171294 <alexander171294@live.com>
2014-12-09 01:32:40 -0300

    Update iHttpClient.php

--- src/iHttpClient.php
+++ src/iHttpClient.php
@@ -1,5 +1,6 @@
 <?php namespace PHPSocketMaster;
 
+
 if(!class_exists('PHPSocketMaster\SocketMaster')) trigger_error('The httpClient require include iSocketMaster interface and SocketMaster class', E_USER_ERROR);
 
 // requerimos la clase socketMaster
