1ca6eaea0cf9fedfa43f67201d6cba591b80df3d
alexander171294 <alexander171294@live.com>
2015-04-07 15:58:29 -0300

    nuevo ejemplo de listen agregado

--- examples/05 make a server/spanish.php
+++ examples/05 make a server/spanish.php
@@ -14,4 +14,77 @@
  *  Si usted aún asi decea crear su servidor, y no tiene intención de utilizar 
  *  PHPServerSocket puede revisar los ejemplos de la carpeta OLD
  *  Y la documentación que se provee en la wiki del proyecto.
+ *  
+ *  Si usted solo decea crear un socket client-client donde el que se encuentre
+ *  a la escucha solo pueda recibir un socket y no más de uno, puede ver el
+ *  siguiente ejemplo.
+ */
+ 
+// socket a la escucha
+class Listener extends PHPSocketMaster/SocketMaster
+{
+	// nunca se ejecutarán estas funciones (se escriben por compatibilidad):
+	public function onConnect() {}
+	public function onDisconnect()	{}
+	public function onReceiveMessage($message) {}
+  public function onSendRequest(&$cancel, $message) {}
+  public function onSendComplete($message) {}
+
+	// cuando ocurre un error
+	public function onError($errorMessage)
+	{
+		echo 'Error ocurrido'.$errorMessage;
+		die(); // finalizamos la ejecución
+	}
+
+	public function onNewConnection(SocketBridge $socket)
+	{
+
+	}
+     
+}
+
+// creamos el receptor
+class receptor extends PHPSocketMaster/SocketEventReceptor
+{
+    // cuando ocurra un error en nuestro cliente
+    public function onError() {}
+    
+    // cuando se logre establecer la conexion recibida
+    public function onConnect() {}
+    
+    // cuando se desconecte
+    public function onDisconnect() {}
+    
+    // cuando se recibe un mensaje
+    public function onReceiveMessage($message) {}
+    
+    // cuando se quiere enviar un mensaje al cliente
+    public function onSendRequest(&$cancel, $message) {}
+    
+    // cuando se envio correctamente el mensaje al cliente
+    public function onSendComplete($message) {}
+    
+    // se ejecuta cada segundo
+    public function onRefresh() {}
+}
+
+/// creamos la instancia pasando por parametro el dominio o ip local y el puerto
+$listener = new Listener('localhost', '2026');
+
+// ponemos a la escucha
+$listener->listen(); 
+
+// creamos el receptor
+$receptor = new receptor();
+
+// refrezcamos hasta recibir la conexión
+while($listener->refreshListen($receptor) == false){}
+
+// refrezcamos el nuevo cliente
+$receptor->refresh_loop();
+
+/** puede crear un socket a la escucha para un websocket agregando un parametro SCKM_WEB y cambiando
+ *  una de las clases, si decea recibir conexiones de websocket por favor revise la documentación
+ *  ubicada en la wiki
  */
