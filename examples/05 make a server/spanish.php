4f96d2056d2d4a0572378a6efd237e72ba61ae30
alexander171294 <alexander171294@live.com>
2015-04-07 15:43:27 -0300

    Ejemplo de sockets a la escucha

---
+++ examples/05 make a server/spanish.php
@@ -0,0 +1,17 @@
+<?php
+
+/** PHPSocketMaster tiene soporte para funciones Listen, ListenRefresh
+ *  y para crear un servidor completo de sockets a la escucha, y hasta para crear
+ *  un servidor mixto de sockets y websockets.
+ *  
+ *  Originalmente incluimos ejemplos para que usted vea como podría ser creado un
+ *  servidor de sockets en php para recibir muchas conexiones.
+ *  
+ *  No obstante, actualmente se está desarrollando PHPServerSocket una librería
+ *  complementaria que facilita muchisimo la creación de servidores, además de 
+ *  proveer los mecanismos necesarios para mantener la estabilidad del servidor.
+ *  
+ *  Si usted aún asi decea crear su servidor, y no tiene intención de utilizar 
+ *  PHPServerSocket puede revisar los ejemplos de la carpeta OLD
+ *  Y la documentación que se provee en la wiki del proyecto.
+ */
